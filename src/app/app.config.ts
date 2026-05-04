0388f23733b4dd590145da3d073e26a966c978eb
STimoshevich <80628922+STimoshevich@users.noreply.github.com>
2026-05-04 10:38:47 +0300

    Provide ngx-daterangepicker config in app providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { NgxDaterangepickerBootstrapModule } from 'ngx-daterangepicker-bootstrap';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(NgxDaterangepickerBootstrapModule.forRoot())
   ]
 };
